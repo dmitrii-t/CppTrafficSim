8ad1e27d3a925987f7ff475e0d6820d813fb8264
Dmitrii Tcimbal <dtcimbal@gmail.com>
2025-02-18 09:22:14 +1100

    Implements TrafficLight::simulate and TrafficLight::cycleThroughPhases methods

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -9,7 +9,6 @@
 // forward declarations to avoid include cycle
 class Vehicle;
 
-
 // FP.3 Define a class „MessageQueue“ which has the public methods send and receive. 
 // Send should take an rvalue reference of type TrafficLightPhase whereas receive should return this type. 
 // Also, the class should define an std::dequeue called _queue, which stores objects of type TrafficLightPhase. 
@@ -29,25 +28,41 @@ private:
 // as well as „TrafficLightPhase getCurrentPhase()“, where TrafficLightPhase is an enum that 
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
+enum class TrafficLightPhase {
+    red,
+    green,
+};
 
-class TrafficLight
+class TrafficLight : TrafficObject
 {
 public:
     // constructor / desctructor
-
+    TrafficLight();
+    
     // getters / setters
-
+    TrafficLightPhase getCurrentPhase();
+    
     // typical behaviour methods
-
+    void simulate() override;
+    void waitForGreen();
+    
 private:
     // typical behaviour methods
-
+    void cycleThroughPhases();
+    void toggleCurrentPhase();
+    
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
     // send in conjunction with move semantics.
 
+    static std::uniform_int_distribution <long long> dis;
+    static std::mt19937 gen;
+    
     std::condition_variable _condition;
     std::mutex _mutex;
+
+    TrafficLightPhase _currentPhase; // <- shared resource
+    
 };
 
 #endif
